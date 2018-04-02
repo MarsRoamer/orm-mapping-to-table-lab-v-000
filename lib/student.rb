9456468230618738e42b8a446f1de157055f0bd2
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-02 19:27:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ class Student
     name = hash[:name]
     grade = hash[:grade]
     student = Student.new(name, grade)
-    save
+    self.save
 
   end
   # Remember, you can access your database connection anywhere in this class
